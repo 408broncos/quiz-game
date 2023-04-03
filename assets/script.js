56ca0a6fe49ebd3cde199f77a9a6a6fec94e1e89
Jordan Cardenas <jordancardenas@Jordans-MBP.attlocal.net>
2023-04-03 15:48:32 -0700

    add new variables to javascript code.

--- assets/script.js
+++ assets/script.js
@@ -67,3 +67,30 @@ const questions = [
 	}
 ];
 
+var timeLeft = 60;
+var timerID;
+var timerEl = document.getElementById("timer");
+var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
+var questionContainerEl = document.getElementById("question-container");
+var startContainerEl = document.getElementById("start-container");
+var questionEl = document.getElementById("question");
+var answerButtonsEl = document.getElementById("answer-buttons");
+var checkAnswerEl = document.getElementById("check-answer");
+var viewHighScores = document.getElementById("highscore-link");
+var submitButton = document.getElementById("submit-btn");
+var clearScoreButton = document.getElementById("clear-btn");
+var nameField = document.getElementById("player-name");
+var restartButton = document.getElementById("restart-btn");
+var scoreField = document.getElementById("player-score");
+var scores = JSON.parse(localStorage.getItem("scores")) || [];
+
+
+
+
+
+
+
+
+
+
