8b7f2b625390163f35693f8f08169a9df235a25e
Jordan Cardenas <jordancardenas@Jordans-MBP.attlocal.net>
2023-04-03 15:12:05 -0700

    add new HTML,CSS and javascript format.

---
+++ assets/script.js
@@ -0,0 +1,69 @@
+const jsq_questions = [
+	{
+		"id": 2,
+		"question": "Which of the following is the correct syntax to redirect a URL using JavaScript?",
+		"options": {
+			"a": "window.location.href",
+			"b": "document.querySelector",
+			"c": "document.getElementById",
+			"d": "location.href"
+		},
+		"answer": "a"
+	},
+	{
+		"id": 2,
+		"question": "How would you start a class element in css?",
+		"options": {
+			"a": "<",
+			"b": "#",
+			"c": ".",
+			"d": "!"
+		},
+		"answer": "c"
+	},
+	{
+		"id": 2,
+		"question": "What is the function to start with when wanting to use a timer?",
+		"options": {
+			"a": "function timer",
+			"b": "function math.floor",
+			"c": "function timerInterval",
+			"d": "function setTime"
+		},
+		"answer": "d"
+	},
+	{
+		"id": 2,
+		"question": "What does HTML stand for?",
+		"options": {
+			"a": "How to Make Language",
+			"b": "Hypertext Markdown Language",
+			"c": "Hypertext Markup Language",
+			"d": "Hitting the Mushy Lake"
+		},
+		"answer": "c"
+	},
+	{
+		"id": 2,
+		"question": "What is the correct element to start with in HTML when wanting to add a picture?",
+		"options": {
+			"a": "<li>",
+			"b": "<link>",
+			"c": "<src>",
+			"d": "<img>"
+		},
+		"answer": "a"
+	},
+	{
+		"id": 2,
+		"question": "Which object is used to hold data that represents a text form?",
+		"options": {
+			"a": "Alert",
+			"b": "String",
+			"c": "Null",
+			"d": "Boolean"
+		},
+		"answer": "b"
+	}
+];
+
